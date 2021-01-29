a64594edbc34ae3b20e079c1a0f0010f05d4ea68
Alessandro <AlessandroCaroti@users.noreply.github.com>
2021-01-29 16:02:26 +0100

    moved data folder

--- project/map/script.js
+++ project/map/script.js
@@ -1,6 +1,9 @@
-console.log("STO CAZZO!");
 // MODIFICARE PER CAMBIARE LE DIMENSIONI DELLA MAPPA
 var w = 800;
 var h = 500;
+
+map_file = "../../data/countries-50m.json"
+
+
 
 svg = d3.select("#svg-map").attr("width", w).attr("height", h);
