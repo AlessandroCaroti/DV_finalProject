f60901ac1fe44b1da1cb7926ad9abbd83f8c7b87
simocampi <37673011+simocampi@users.noreply.github.com>
2021-01-29 20:55:28 +0100

    linechart

--- project/LineChart/script.js
+++ project/LineChart/script.js
@@ -1,3 +1,54 @@
+function tooltip(svg){
+
+      var focus = svg.append("g")
+      .attr("class", "focus")
+      .style("display", "none");
+
+      focus.append("circle")
+      .attr("r", 5);
+
+      focus.append("rect")
+      .attr("class", "tooltip")
+      .attr("width", 100)
+      .attr("height", 50)
+      .attr("x", 10)
+      .attr("y", -22)
+      .attr("rx", 4)
+      .attr("ry", 4);
+
+      focus.append("text")
+      .attr("class", "tooltip-date")
+      .attr("x", 18)
+      .attr("y", -2);
+
+      focus.append("text")
+      .attr("x", 18)
+      .attr("y", 18)
+      .text("Likes:");
+
+      focus.append("text")
+      .attr("class", "tooltip-likes")
+      .attr("x", 60)
+      .attr("y", 18);
+
+      return focus
+
+}
+
+function mousemove(svg, x, y) {
+  
+  var focus = tooltip(svg);
+  
+  var x0 = x.invert(d3.mouse(this)[0]),
+      i = bisectDate(data, x0, 1),
+      d0 = data[i - 1],
+      d1 = data[i],
+      d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+  focus.attr("transform", "translate(" + x(d.date) + "," + y(d.likes) + ")");
+  focus.select(".tooltip-date").text(dateFormatter(d.date));
+  focus.select(".tooltip-likes").text(formatValue(d.likes));
+}
+
 
   function changeData() {
    
@@ -5,60 +56,54 @@
    
     //test
     dataFile = "anscombe_I"
- 
+           //select svg
+    var margin = {top: 10, right: 30, bottom: 30, left: 60},
+          
+    width = 500 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+    
+    var svg = d3.select("#line_chart_graph")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            
     d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv")
-      .then((data) => {
-          
-          var margin = {top: 10, right: 30, bottom: 30, left: 60},
-          
-          width = 500 - margin.left - margin.right,
-          height = 500 - margin.top - margin.bottom;
+      .then(  (data) => {
 
           data.forEach(d => {
-            console.log(d.date)
             d.date = d3.timeParse("%Y-%m-%d")(d.date) 
-            d.value = d.value;
-            console.log(d.date)
+            d.value = d.value;   
           });
-        
           
-        //select svg
-        var svg = d3.select("#line_chart_graph")
-          .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-          .append("g")
-            .attr("transform",
-                  "translate(" + margin.left + "," + margin.top + ")");
-        
-        // Now I can use this dataset:
-        data_ =>{
-          // Add X axis --> it is a date format
-          var x = d3.scaleTime()
-            .domain(d3.extent(data_, function(d) { return d.date; }))
+            // Add X axis --> it is a date format
+            var x = d3.scaleTime()
+            .domain(d3.extent(data, function(d) { return d.date; }))
             .range([ 0, width ]);
-          svg.append("g")
+        
+        svg.append("g")
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(x));
 
           // Add Y axis
           var y = d3.scaleLinear()
-            .domain([0, d3.max(data_, function(d) { return +d.value; })])
+            .domain([0, d3.max(data, function(d) { return +d.value; })])
             .range([ height, 0 ]);
-          svg.append("g")
-            .call(d3.axisLeft(y));
-
-          // Add the line
-          svg.append("path")
-            .datum(data_)
+        svg.append("g")
+            .call(d3.axisLeft(y))
+        
+            // Add the line
+        svg.append("path")
+            .datum(data)
             .attr("fill", "none")
             .attr("stroke", "steelblue")
             .attr("stroke-width", 1.5)
             .attr("d", d3.line()
-              .x(function(d) { return x(d.date) })
-              .y(function(d) { return y(d.value) })
-              ) 
-        }
+            .x(function(d) { return x(d.date) })
+            .y(function(d) { return y(d.value) })
+        )
+
+        
        })
        .catch((error) =>{
           console.log(error);
@@ -106,3 +151,6 @@ function click_tab_stripechart(evt, graphic_name) {
   document.getElementById(graphic_name).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+
+
