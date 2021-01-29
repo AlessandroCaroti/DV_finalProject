53e08c88e846afbee71c29dd582d063aedba0ddb
simocampi <Sim0ne1997>
2021-01-29 16:49:03 +0100

    tab menu

--- project/LineChart/script.js
+++ project/LineChart/script.js
@@ -1,1 +1,18 @@
-console.log("STO CAZZO!");
+
+
+function click_tab(evt, cityName) {
+    
+    document.getElementById("starting_tab_content").style.display = "none"
+
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
