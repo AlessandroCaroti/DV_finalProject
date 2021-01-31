bfe4cd384cfbfc2e168e33d4ba22af3e8841af95
Alessandro <AlessandroCaroti@users.noreply.github.com>
2021-01-31 10:15:15 +0100

    Update script.js

--- project/map/script.js
+++ project/map/script.js
@@ -1,13 +1,18 @@
-console.log("STO CAZZO");
+// *************************************************** //
+//                START GLOBAL VARIABLE                //
 
-// MODIFICARE PER CAMBIARE LE DIMENSIONI DELLA MAPPA
+// MAP DIMANSION
 var w = 1000;
 var h = 500;
 
 var projection;
 var map_container;
+var colorsRange = ["rgb(5, 48, 97)", "white", "rgb(103, 0, 31)"];
+//var colorsRange = ["blue", "white", "red"];
 
 var geoGenerator;
+
+var tmp_data;
 
 var zoom = d3
   .zoom()
@@ -16,7 +21,12 @@ var zoom = d3
   })
   .scaleExtent([1, 8]);
 
+// FILES & DIRECTORY PATH VARIABLE
 map_file = "../../data/countries-50m.json";
+tmp_file = "../../data/data_year/2019/Annual_mean.csv";
+
+//                END GLOBAL VARIABLE                //
+// ************************************************* //
 
 function drawMap(world) {
   console.log("DRAW-MAP");
@@ -38,6 +48,9 @@ function drawMap(world) {
     .enter()
     .append("path")
     .attr("class", "country")
+    .attr("id", (d) => {
+      return d.properties.name;
+    })
     .attr("d", geoGenerator);
 
   // Draw gridlines
@@ -46,7 +59,30 @@ function drawMap(world) {
   var maps = map_container.selectAll("path.grat_2").data(graticule.lines());
   maps.enter().append("path").classed("grat_2", true).attr("d", geoGenerator);
 
+  //Associate to each county a color proportionate to it's anomaly
+  update_colors();
+
+  //Define the events of the countries(Es, mouseover, click...)
   country_events();
+}
+
+function update_colors() {
+  //GLOBAL MIN & MAX
+  //[-2.5168333333333335, 3.1985000000000006] [-2.5169, 0, +3.1986]
+
+  var colorScale = d3
+    .scaleLinear()
+    //.domain([-5, 0, +5])
+    .domain([-2.5169, 0, +3.1986])
+    .range(colorsRange);
+
+  tmp_data.forEach(function (d) {
+    var element = document.getElementById(d.Country);
+
+    if (typeof element != "undefined" && element != null) {
+      d3.select(element).style("fill", colorScale(d["ANOMALY"]));
+    }
+  });
 }
 
 function country_events() {
@@ -89,15 +125,14 @@ function country_selected(country) {
 
 // LOAD COUNTRIES MENU
 
+var countries = d3
+  .select("datalist#countryList")
+  .data(["Internet Explorer", "Firefox", "Chrome", "Opera", "Safari"]);
 
-var countries = d3.select("datalist#countryList")
-        .data([("Internet Explorer"), ("Firefox"), ("Chrome"), ("Opera"), ("Safari")]);
-        
-countries.enter()
-          .append("option")
-          .attr("value", d => d);
-
-
+countries
+  .enter()
+  .append("option")
+  .attr("value", (d) => d);
 
 // UPDATE YEAR
 
@@ -112,27 +147,48 @@ slider.oninput = function () {
 
 // UPDATE COUNTRY
 
-function changeCountry(){
-  console.log("bubi")
+function changeCountry() {
+  console.log("bubi");
 }
 
-// *******************************************************************
-// FILES LOADING
+// ******************************************** //
+//                FILES LOADING                //
 
-d3.json(map_file)
-  .then((world) => {
-    console.log("LOAD MAP");
+function load_tempYear(temp_file) {
+  d3.csv(temp_file)
+    .then(function (data) {
+      console.log("LOAD TEMP");
 
-    let topology = world;
-    topology = topojson.presimplify(topology);
-    topology = topojson.simplify(topology, 0.05);
+      data.forEach((d) => {
+        d.ANOMALY = parseFloat(d.Anomaly);
+      });
+      tmp_data = data;
+      load_map();
+    })
+    .catch(function (error) {
+      console.log(error);
+      throw error;
+    });
+}
 
-    drawMap(topology);
-  })
-  .catch((error) => {
-    console.log(error);
-    throw error;
-  });
+function load_map() {
+  d3.json(map_file)
+    .then((world) => {
+      console.log("LOAD MAP");
 
+      let topology = world;
+      topology = topojson.presimplify(topology);
+      topology = topojson.simplify(topology, 0.05);
 
+      drawMap(topology);
+    })
+    .catch((error) => {
+      console.log(error);
+      throw error;
+    });
+}
 
+// ******************************************** //
+//              DOVE INIZIA TUTTO              //
+
+load_tempYear(tmp_file);
