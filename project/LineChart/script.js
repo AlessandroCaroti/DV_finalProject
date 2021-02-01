57233ad7baca78c336f2a3633bbaad49f7b7b2ab
simocampi <Sim0ne1997>
2021-02-01 16:50:15 +0100

    Tooltip with line

--- project/LineChart/script.js
+++ project/LineChart/script.js
@@ -3,7 +3,11 @@ var full_width = 900
 var margin = {top: 40, right: 70, bottom: 30, left: 50};
 var width = full_width - margin.left - margin.right;
 var height = full_width*9/16 - margin.top - margin.bottom;
-var parseTime = d3.timeParse("%Y");
+var parseTime = d3.timeParse("%Y-%m");
+
+var tooltip = d3.select("body")
+                .append("div")
+                .attr("class", "tooltip");
 
 var baseline;
 var baseline_unc;
@@ -32,7 +36,7 @@ function parseDataAttributes(data){
   
   data.forEach(d => {
       
-    d.date = parseTime(d.Year);
+    d.date = parseTime(d.Year+"-"+d.Month);
     d.annual_value = baseline + parseFloat(d["Annual Anomaly"]);
     d.annual_unc =parseFloat(d["Annual Unc."]);
     d.ten_years_value =  baseline + parseFloat(d["Ten-year Anomaly"])
@@ -46,11 +50,10 @@ function tooltip_circle_enter(self, event, d, tooltip_div, svg, x){
     
   tooltip_div.transition().duration(200);
 
-  const date = x.invert(d3.mouse(tipBox.node())[0]);
-  item = data.find( d => Math.abs( d.date - date ) < 1000*60*60*24*16 );
+  var date = x.invert(d3.mouse(tipBox.node())[0]);
+  item = data.find( d => d.date - date  == 0 );
   
-  const tooltipLine = svg.append('line');
-  
+ 
   
   tooltipLine.attr('stroke', 'black')
       .attr('x1', x(date))
@@ -98,17 +101,53 @@ function drawCircles(svg, data, x, y){
                       })
                   .on("mouseleave", function(){
                         tooltip_circle_leave(this, tooltip)
-                      })
-                  
+                      })              
     
-  return circle;
+    return circle;
   }
 
+function removeTooltip(tooltipLine) {
+  
+  if (tooltip) tooltip.style('display', 'none');
+  if (tooltipLine) tooltipLine.attr('stroke', 'none');
+}
+
+function drawTooltip(self, event, x, data, tooltipLine) {
+  
+  const date = x.invert(d3.pointer(event, self.node())[0]);
+  
+
+  //find element 
+  var elem = data.find( (d) =>  Math.abs( d.date - date ) < 1000*60*60*24*16 );
+
+
+  tooltipLine.attr('stroke', 'black')
+      .attr('x1', x(date))
+      .attr('x2', x(date))
+      .attr('y1', 0)
+      .attr('y2', height);
+  
+
+  var tipText =  String(
+    "<b> Year: " + elem.date.getFullYear()+"<br/>" +"<br/>" +
+    "Annual Average Temperature: "+elem.annual_value.toFixed(2) +" &deg;C " +
+    " &plusmn; " +  elem.annual_unc.toFixed(2) + " "+
+    "<br/>" +"<br/>" +
+    "Ten Years Average Temperature: "+elem.ten_years_value.toFixed(2) +" &deg;C " +
+    " &plusmn; " +  elem.ten_years_unc.toFixed(2) + "</b>  "
+  )
+     
+  tooltip.html("")
+      .style('display', 'block')
+      .style('left', String( (event.pageX) + 20) + "px" )
+      .style('top', String( (event.pageY) - 20) + "px" )
+      .append('div')
+      .html( tipText );
+}
 
  
   
-
-  function tooltip_circle_leave(self, tooltip_div){
+function tooltip_circle_leave(self, tooltip_div){
     
     d3.select(self).classed("selected_circle", false)
     tooltip_div.transition()
@@ -117,7 +156,7 @@ function drawCircles(svg, data, x, y){
   }
 
 
-  function updateAxis(x_axis_class, y_axis_class, x, y){
+function updateAxis(x_axis_class, y_axis_class, x, y){
     
                 // update  x Axis
       d3.select(x_axis_class)
@@ -183,18 +222,21 @@ function drawCircles(svg, data, x, y){
               .attr("class","line_chart_ten_years")
               .attr("d", valueline_ten_years);       
       
-        
-        
-    //drawCircles(svg, data, x, y);
+    var tooltipLine = svg.append('line').attr("class","line_tip");
     
-  
-   
+    var tipBox = svg.append('rect')
+                    .attr('width', width)
+                    .attr('height', height)
+                    .attr('opacity', 0)
+                    .attr('id','tipbox')
+                    .on('mousemove', (event) => drawTooltip(tipBox, event, x, data, tooltipLine))
+                    .on('mouseout', () => removeTooltip(tooltipLine));
+
   }
 
 
-  function changeData() {
+function changeData() {
     
-    parseTime = d3.timeParse("%Y");
     // prendere dati da mappa selezionata o dropdown menu
     var dataFile = document.getElementById('dataset').value;
     document.getElementById("country_t").innerHTML= dataFile;
@@ -207,8 +249,6 @@ function drawCircles(svg, data, x, y){
     else
         folder = dataFile;
     
-    console.log("Folder: ", folder, "\nDatafile: ", dataFile);
-   
    
     var csv = "/../../data/data_temp/"+folder+"/"+dataFile+"_anomalyTable.csv"
  
@@ -239,9 +279,7 @@ function drawCircles(svg, data, x, y){
                                     .x(function(d) { return x(d.date); })
                                     .y(function(d) { return y(d.ten_years_value ); })
                                     .defined( (d) => { return ( !isNaN(d.ten_years_value ) ) } );        
-                         
-        
-                  
+                                      
         // Draw the line the line
         svg.select(".line_chart_annual")
           .data([data])
@@ -252,16 +290,16 @@ function drawCircles(svg, data, x, y){
           .data([data])
           .attr("d", valueline_ten_years)
           .transition().duration();      
-                                        
-       
-        //remove old circles and update
-        //svg.selectAll(".scatter").remove();
-        //drawCircles(svg, data, x, y);  
+
+          var tooltipLine = d3.select(".line_tip");
+          var tipBox = d3.select("#tipbox")
+                         .on('mousemove', (event) => drawTooltip(tipBox, event, x, data, tooltipLine))
+                         .on('mouseout', () => removeTooltip(tooltipLine));
+                     
          
       })
       .catch((error) =>{
         console.log(error);
-        //alert("Unable To Load The Dataset!!");
         throw(error);
     })  
 }
