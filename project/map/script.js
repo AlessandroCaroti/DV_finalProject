e8a3e066e36f420f7ff047c65fae11ee700206f2
Alessandro <AlessandroCaroti@users.noreply.github.com>
2021-02-03 17:53:00 +0100

    Update script.js
    possibile fix

--- project/map/script.js
+++ project/map/script.js
@@ -115,9 +115,8 @@ function country_events() {
     d3.select(".selected_country").classed("selected_country", false);
 
     d3.select(this).classed("selected_country", true);
-    console.log(b.properties.name);
-    console.log(this.id);
-    country_selected(b);
+    console.log(this.id, this);
+    country_selected(this.__data__);
   });
 }
 
