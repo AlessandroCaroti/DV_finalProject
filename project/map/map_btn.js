d0a25e603ba6674e10cbcc6f4a2fcf17ceb40459
Alessandro <AlessandroCaroti@users.noreply.github.com>
2021-02-06 01:12:25 +0100

    Update map_btn.js

--- project/map/map_btn.js
+++ project/map/map_btn.js
@@ -5,9 +5,7 @@ var r = 5;
 /* -------------------------------------------------------------------------------------- */
 /*                       ZOOM IN                                                          */
 var zoom_in = d3.select("#zoom-in");
-zoom_in
-  .select("path")
-  .attr("d", topRoundedRect(0, 0, width, height, r));
+zoom_in.select("path").attr("d", topRoundedRect(0, 0, width, height, r));
 zoom_in
   .append("line")
   .attr("x1", width / 2 - 7)
@@ -31,9 +29,7 @@ zoom_in
 /* -------------------------------------------------------------------------------------- */
 /*                       ZOOM OUT                                                         */
 var zoom_out = d3.select("#zoom-out");
-zoom_out
-  .select("path")
-  .attr("d", bottomRoundedRect(0, 0, width, height, r));
+zoom_out.select("path").attr("d", bottomRoundedRect(0, 0, width, height, r));
 zoom_out
   .append("line")
   .attr("x1", width / 2 - 7)
@@ -47,25 +43,32 @@ zoom_out
 /* -------------------------------------------------------------------------------------- */
 /*                       ZOOM RESET                                                       */
 var zoom_reset = d3.select("#zoom-reset");
-zoom_reset
-  .select("rect")
-  .attr("width", width)
-  .attr("height", height);
+zoom_reset.select("rect").attr("width", width).attr("height", height);
 
-zoom_reset
+var inner_circle = zoom_reset
   .append("circle")
   .attr("r", 3)
   .attr("cx", width / 2)
   .attr("cy", height / 2)
+  .attr("fill", "black")
   .attr("pointer-events", "none");
 
-
-zoom_reset
+var outer_circle = zoom_reset
   .append("circle")
-  .attr("r", 9)
+  .attr("r", 10)
   .attr("cx", width / 2)
   .attr("cy", height / 2)
   .style("fill", "none")
   .style("stroke", "black")
   .style("stroke-width", 1)
   .attr("pointer-events", "none");
+
+function no_zoom() {
+  inner_circle.attr("r", 3);
+  outer_circle.style("stroke-width", 1);
+}
+
+function local_zoom() {
+  inner_circle.attr("r", 2);
+  outer_circle.style("stroke-width", 3);
+}
