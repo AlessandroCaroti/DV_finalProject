8b1827ad871cd00848cdf6c29d57cb75632a8ef7
simocampi <Sim0ne1997>
2021-02-18 18:33:21 +0100

    s

--- project/Charts/TableMeanRate/drawTable.js
+++ project/Charts/TableMeanRate/drawTable.js
@@ -1,185 +1,116 @@
-
 var continent, portion_continent, hemisphere;
 
+function loadData_table() {
+  //ciaovar dataset = "";
+  d3.csv(countries).then((data) => {
+    var i = 0;
+    data.forEach((d) => {
+      var dropdown = document.getElementById("dataset");
+      var option = document.createElement("option");
+      option.setAttribute("value", d.Country);
+      option.innerHTML = d.Country;
+      dropdown.append(option);
+      if (option.value == "Italy") {
+        dropdown.selectedIndex = i;
+        dropdown.options[i].selected = true;
+        // set Italy default dataset
+        dataset = option.value;
+      }
+      i++;
+    });
 
-
-function loadData_table(){ 
-    
-    var dataset = "";
-    d3.csv(countries)
-      .then((data)=>{
-
-          var i = 0;
-          data.forEach( d => {
-
-            var dropdown = document.getElementById("dataset");   
-            var option =  document.createElement("option");
-            option.setAttribute("value", d.Country);
-            option.innerHTML = d.Country;
-            dropdown.append(option)
-            if( option.value == "Italy"){
-              dropdown.selectedIndex = i;
-              dropdown.options[i].selected = true;
-              // set Italy default dataset
-              dataset = option.value;
-              
-            }
-            i++;
-          });
-       
-        default_dataset(dataset);
-    })
-
+    default_dataset(dataset);
+  });
 }
 
 
 
 
-function default_dataset(dataFile=""){
-
-    if( dataFile == "") dataFile = "Afghanistan";
-  
-    
-    var dataFile = document.getElementById('dataset').value;
-    document.getElementById("table_country").innerHTML= dataFile;
-    
-    var folder;
-    
-    if( dataFile.charAt(dataFile.length  - 1) == '.' ) folder = dataFile.slice(0,-1);
-    else
-        folder = dataFile;
-
-    initBaselineAndInfo(dataFile)
-   
-
-    var csv_country = "/../../remaining_data/data_new/"+folder+"/"+dataFile+"_anomalyTable.csv";
-
-    d3.csv(csv_country)
-      .then( (data_country) =>{
-
-      
-        parseDataAttributes(data_country,dataFile);
-    
-    
-        var csv_global = "/../../remaining_data/general_data/global-land/global-land_anomalyTable.csv";
-        d3.csv(csv_global)
-          .then( (data_global) =>{
-
-                parseDataAttributes(data_global,"global-land");
-           
-                d3.json("/../../remaining_data/data_new/"+folder+"/"+dataFile+"_info.json")
-                .then( (info) =>{
-
-                   
-                  var continent = info["continent"];
-                  var portion_continent = info["portion-continent"];
-                  var hemisphere = info["hemisphere"];
-                  
-                  //case continet not null
-                
-                  if( !isInfoNaN(continent) && !isInfoNaN(hemisphere) && !isInfoNaN(portion_continent))
-                      readDataAllNonNull(continent,portion_continent,hemisphere, data_country, data_global)
-                  
-                  if( isInfoNaN(continent) && isInfoNaN(hemisphere) && isInfoNaN(portion_continent))
-                      readDataAllNull( data_country, data_global);
-                  
-                  if( !isInfoNaN(continent) && isInfoNaN(hemisphere) && isInfoNaN(portion_continent)) readDataOnlyContinent(continent, data_country, data_global);  
-                  if( isInfoNaN(continent) && isInfoNaN(hemisphere) && !isInfoNaN(portion_continent)) readDataOnlyPortionContinent(portion_continent, data_country, data_global);
-                  if( isInfoNaN(continent) && !isInfoNaN(hemisphere) && isInfoNaN(portion_continent)) readDataOnlyContinent(hemisphere, data_country, data_global);
-                  
-                  if(isInfoNaN(continent) && !isInfoNaN(hemisphere) && !isInfoNaN(portion_continent)) readDataContinentNull(portion_continent,hemisphere, data_country, data_global);
-                  if(!isInfoNaN(continent) && isInfoNaN(hemisphere) && !isInfoNaN(portion_continent)) readDataHemisphereNull(continent,portion_continent, data_country, data_global);
-                  if(!isInfoNaN(continent) &&  !isInfoNaN(hemisphere) && isInfoNaN(portion_continent)) readDataPortionContinentNull(continent,hemisphere, data_country, data_global);
-  
-
-            })
-              .catch((error) =>{
-                console.log(error);
-                throw(error);
-              })
-          }) .catch((error) =>{
-            console.log(error);
-            throw(error);
-          })
-    
-        }) .catch((error) =>{
-          console.log(error);
-          //alert("Unable To Load The Dataset!!");
-          throw(error);
-        })    
-           
-}
 
 
-function changeDataTable(){
-  
-     // prendere dati da mappa selezionata o dropdown menu
-     var dataFile = document.getElementById('dataset').value;
-     document.getElementById("table_country").innerHTML= dataFile;
-     
-     initBaselineAndInfo(dataFile);
 
-     var folder;
-  
-     if( dataFile.charAt(dataFile.length  - 1) == '.' ) folder = dataFile.slice(0,-1);
-     else
-         folder = dataFile;
-     
-    //var csv = "/../../data/data_temp/"+folder+"/"+dataFile+"_anomalyTable.csv"
-    var csv_country = "/../../remaining_data/data_new/"+folder+"/"+dataFile+"_anomalyTable.csv";
+function default_dataset(dataFile = "") {
+  if (dataFile == "") dataFile = "Afghanistan";
 
-    //Di default c'è dataset 1
-    d3.csv(csv_country)
-      .then( (data_country) =>{
+  var dataFile = document.getElementById("dataset").value;
+  document.getElementById("table_country").innerHTML = dataFile;
+  var folder;
+  if (dataFile.charAt(dataFile.length - 1) == ".")
+    folder = dataFile.slice(0, -1);
+  else folder = dataFile;
 
-      
-        parseDataAttributes(data_country,dataFile);
-    
-    
-        var csv_global = "/../../remaining_data/general_data/global-land/global-land_anomalyTable.csv";
-        d3.csv(csv_global)
-          .then( (data_global) =>{
+  initBaselineAndInfo(dataFile);
 
+ 
+  var csv_country =
+    "/../data/counties/" + folder + "/" + dataFile + "_anomalyTable.csv";
 
-              parseDataAttributes(data_global,"global-land");
-           
-              
-                d3.json("/../../remaining_data/data_new/"+folder+"/"+dataFile+"_info.json")
-                .then( (info) =>{
+  d3.csv(csv_country)
+    .then((data_country) => {
+      parseDataAttributes(data_country, dataFile);
+      createEmptyTable(data_country)
 
-                   
-                  var continent = info["continent"];
-                  var portion_continent = info["portion-continent"];
-                  var hemisphere = info["hemisphere"];
-                  
-                  //case continet not null
-                 
-                  if( !isInfoNaN(continent) && !isInfoNaN(hemisphere) && !isInfoNaN(portion_continent))
-                      readDataAllNonNull(continent,portion_continent,hemisphere, data_country, data_global,true)
+      d3.json("/../data/counties/" + folder + "/" + dataFile + "_info.json")
+        .then((info) => {
+          var generalization_list = info["Generalization"];
+          console.log(generalization_list);
 
-                  if( isInfoNaN(continent) && isInfoNaN(hemisphere) && isInfoNaN(portion_continent))
-                      readDataAllNull( data_country, data_global, true)
-                  
-                  if( !isInfoNaN(continent) && isInfoNaN(hemisphere) && isInfoNaN(portion_continent)) readDataOnlyContinent(continent, data_country, data_global,true);  
-                  if( isInfoNaN(continent) && isInfoNaN(hemisphere) && !isInfoNaN(portion_continent)) readDataOnlyPortionContinent(portion_continent, data_country, data_global,true);
-                  if( isInfoNaN(continent) && !isInfoNaN(hemisphere) && isInfoNaN(portion_continent)) readDataOnlyContinent(hemisphere, data_country, data_global,true);
-                  
-                  if(isInfoNaN(continent) && !isInfoNaN(hemisphere) && !isInfoNaN(portion_continent)) readDataContinentNull(portion_continent,hemisphere, data_country, data_global,true);
-                  if(!isInfoNaN(continent) && isInfoNaN(hemisphere) && !isInfoNaN(portion_continent)) readDataHemisphereNull(continent,portion_continent, data_country, data_global,true);
-                  if(!isInfoNaN(continent) &&  !isInfoNaN(hemisphere) && isInfoNaN(portion_continent)) readDataPortionContinentNull(continent,hemisphere, data_country, data_global,true);                
+          readData(generalization_list, update = false);
 
-            })
-              .catch((error) =>{
-                console.log(error);
-                throw(error);
-              })
-          }) .catch((error) =>{
-            console.log(error);
-            throw(error);
-          })
-    
-        }) .catch((error) =>{
-          console.log(error);
-          throw(error);
         })
+        .catch((error) => {
+          console.log(error);
+          throw error;
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+      //alert("Unable To Load The Dataset!!");
+      throw error;
+    });
+}
+
+function changeDataTable() {
+  // prendere dati da mappa selezionata o dropdown menu
+  if (dataFile == "") dataFile = "Afghanistan";
+
+  var dataFile = document.getElementById("dataset").value;
+  document.getElementById("table_country").innerHTML = dataFile;
+  var folder;
+  if (dataFile.charAt(dataFile.length - 1) == ".")
+    folder = dataFile.slice(0, -1);
+  else folder = dataFile;
+
+  initBaselineAndInfo(dataFile);
+
+  d3.select(".table_mean_rate").selectAll("tr").remove();
+  d3.select(".table_mean_rate").selectAll("td").remove();
+
+  var csv_country =
+    "/../data/counties/" + folder + "/" + dataFile + "_anomalyTable.csv";
+
+  d3.csv(csv_country)
+    .then((data_country) => {
+   
+      createEmptyTable(data_country)
+      parseDataAttributes(data_country, dataFile);
+
+      d3.json("/../data/counties/" + folder + "/" + dataFile + "_info.json")
+        .then((info) => {
+          var generalization_list = info["Generalization"];
+          console.log(generalization_list);
+
+          readData(generalization_list, true);
+
+        })
+        .catch((error) => {
+          console.log(error);
+          throw error;
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+      //alert("Unable To Load The Dataset!!");
+      throw error;
+    });
 }
