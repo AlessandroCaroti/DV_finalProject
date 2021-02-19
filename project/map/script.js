c0d008491b6f66c405fba850cecd6473a3f9b16b
LazyRac00n <37672349+LazyRacc00n@users.noreply.github.com>
2021-02-19 14:13:30 +0100

    Bug fix hovering and pan
    now right at the end of zoom and pan the tooltip is shown only if a country is highlighted

--- project/map/script.js
+++ project/map/script.js
@@ -235,8 +235,12 @@ var zoom = d3
     update_strokes(curr_zoomScale);
   })
   .on("end", function(event){
-    // show tooltip
-    d3.select(".tooltip-map").style("display", "block");
+    // show tooltip if hovering a country
+    let cur_country = map_container.select(".highlighted_country");
+
+    console.log(cur_country);
+    if(!cur_country.empty())
+      d3.select(".tooltip-map").style("display", "block");
 
   });
 
