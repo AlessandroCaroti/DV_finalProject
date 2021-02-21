20186096a649fcfaea529de86ef6d656d21707cf
Alessandro <AlessandroCaroti@users.noreply.github.com>
2021-02-21 11:25:53 +0100

    update center button icons

--- project/map/map_btn.js
+++ project/map/map_btn.js
@@ -5,7 +5,9 @@ var r = 5;
 /* -------------------------------------------------------------------------------------- */
 /*                       ZOOM IN                                                          */
 var zoom_in = d3.select("#zoom-in");
-zoom_in.select("path").attr("d", topRoundedRect(0, 0, width_btn, height_btn, r));
+zoom_in
+  .select("path")
+  .attr("d", topRoundedRect(0, 0, width_btn, height_btn, r));
 zoom_in
   .append("line")
   .attr("x1", width_btn / 2 - 7)
@@ -29,7 +31,9 @@ zoom_in
 /* -------------------------------------------------------------------------------------- */
 /*                       ZOOM OUT                                                         */
 var zoom_out = d3.select("#zoom-out");
-zoom_out.select("path").attr("d", bottomRoundedRect(0, 0, width_btn, height_btn, r));
+zoom_out
+  .select("path")
+  .attr("d", bottomRoundedRect(0, 0, width_btn, height_btn, r));
 zoom_out
   .append("line")
   .attr("x1", width_btn / 2 - 7)
@@ -47,30 +51,59 @@ zoom_reset.select("rect").attr("width", width_btn).attr("height", height_btn);
 
 var inner_circle = zoom_reset
   .append("circle")
-  .attr("r", 1)
+  .attr("r", 5)
   .attr("cx", width_btn / 2)
   .attr("cy", height_btn / 2)
   .attr("fill", "black")
-  .attr("pointer-events", "none");
+  .attr("pointer-events", "none")
+  .attr("visibility", "hidden");
+
+var radius = 7.5
+var s = 3.4 
+var ha = "M " + (width_btn / 2) + " "+ ((height_btn / 2)-radius-s)
+        + "v " + s
+        + "a" + radius + "," + radius + " 0 0 1 " + (+radius) + "," + (+radius)
+        + "h " + s
+        + "h " + (-s)
+        + "a" + radius + "," + radius + " 0 0 1 " + (-radius) + "," + (+radius)
+        + "v "+ s
+        + "v "+ (-s)
+        + "a" + radius + "," + radius + " 0 0 1 " + (-radius) + "," + (-radius)
+        + "h " + (-s)
+        + "h " + s
+        + "a" + radius + "," + radius + " 0 0 1 " + (+radius) + "," + (-radius);
 
 var outer_circle = zoom_reset
-  .append("circle")
-  .attr("r", 10)
-  .attr("cx", width_btn / 2)
-  .attr("cy", height_btn / 2)
-  .style("fill", "none")
+  .append("path")
+  .attr("d", ha)
+  .attr("fill", "none")
   .style("stroke", "black")
-  .style("stroke-width", 3)
-  .attr("pointer-events", "none");
+  .style("stroke-width", 1.3)
+  .attr("pointer-events", "none")
+  .attr("visibility", "hidden");
+
+
+var globe_img = zoom_reset.append("svg:image")
+                      .attr("xlink:href", "/project/images/globe_2.svg")
+                      .attr("pointer-events", "none")
+                      .attr("width", width_btn - (s*2))
+                      .attr("height", height_btn - (s*2))
+                      .attr("x", s)
+                      .attr("y", s);
 
 function no_zoom() {
-  inner_circle.attr("r", 4);
-  outer_circle.style("stroke-width", 1);
+  inner_circle.attr("visibility", "visible");
+  outer_circle.attr("visibility", "visible");
+
+  globe_img.attr("visibility", "hidden");
 }
 
 function local_zoom() {
-  inner_circle.attr("r", 1);
-  outer_circle.style("stroke-width", 3);
+  inner_circle.attr("visibility", "hidden");
+  outer_circle.attr("visibility", "hidden");
+
+  globe_img.attr("visibility", "visible");
+  
 }
 
 /* -------------------------------------------------------------------------------------- */
@@ -85,7 +118,10 @@ animation
 
 var play_ico = animation
   .append("path")
-  .attr("d", roundedTriangle(width_btn / 2 - l + 2, width_btn / 2 - l, l * 2, l * 2))
+  .attr(
+    "d",
+    roundedTriangle(width_btn / 2 - l + 2, width_btn / 2 - l, l * 2, l * 2)
+  )
   .attr("fill", "balck")
   .attr("visibility", "hidden")
   .attr("pointer-events", "none");
