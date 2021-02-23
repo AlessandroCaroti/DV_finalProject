616de157623af39ef57e13befe60d6dd69f9516e
Alessandro <AlessandroCaroti@users.noreply.github.com>
2021-02-23 11:54:43 +0100

    implemented  animation speed option

--- project/utils/dropbown_test.js
+++ project/utils/dropbown_test.js
@@ -42,6 +42,58 @@ d3.select("#text_setting").attr("x", -w2/2).attr("y", y+(h1/2)+5);
 d3.select("#text_animation").attr("x", -w2+w1+10).attr("y", 70);
 d3.select("#text_speed").attr("x", -w2+w1+20).attr("y", 83);
 
+//ANIMATION SPEED
+d3.select("#text_animation").attr("x", -w2+w1+10).attr("y", 70);
+d3.select("#text_speed").attr("x", -w2+w1+10).attr("y", 83);
+const p_l = 42
+const y_l = 94
+const start_line = (-w2+w1+p_l)
+const end_line = (w1-p_l)
+d3.select("#speed_line").attr("x1",start_line).attr("y1",y_l).attr("x2",end_line).attr("y2",y_l);
+const speed_options = [0.5, 1, 2, 4, 8];
+var selected_opt_idx = 1;
+const step = ((end_line-start_line)/(speed_options.length-1));
+for(var i = 0; i<speed_options.length; i++){
+  k = start_line + step*i;
+  d3.select("#animation_speed_group")
+    .append("line")
+    .attr("x1",k)
+    .attr("y1",y_l-1)
+    .attr("x2",k)
+    .attr("y2",y_l+8)
+    .style("stroke", "black")
+    .style("stroke-width", 1.5)
+    .attr("visibility", "hidden");
+
+  d3.select("#animation_speed_group")
+    .append("text")
+    .attr("x", k-5)
+    .attr("y", y_l+17)
+    .attr("font-size", 10)
+    .attr("font-family","Courier New")
+    .text("x"+(speed_options[i]))
+    .attr("visibility", "hidden");
+}
+d3.select("#slider_ball").attr("cx",((start_line) + step*1)).attr("cy",y_l).attr("visibility", "hidden").raise();
+
+d3.select("#decrease_speed").attr("d", "M "+(start_line-22)+" "+y_l+"h "+9)
+  .style("stroke-width", 2).attr("visibility", "hidden").attr("pointer-events", "none");
+d3.select("#decrease_back").attr("cx",(start_line-22)+4.5).attr("cy",y_l).attr("r",8).attr("visibility", "hidden")
+  .on("click", function(){
+    if (selected_opt_idx > 0)
+      selected_opt_idx--;
+      d3.select("#slider_ball").attr("cx", (start_line + step*selected_opt_idx));
+  });
+
+d3.select("#increase_speed").attr("d", "M "+(end_line+22)+" "+y_l+"h "+(-9)+"h "+4.5+"v "+4.5+"v "+(-9))
+  .style("stroke-width", 2).attr("visibility", "hidden").attr("pointer-events", "none");
+d3.select("#increase_back").attr("cx",(end_line+22)-4.5).attr("cy",y_l).attr("r",8).attr("visibility", "hidden")
+  .on("click", function(){
+    if (selected_opt_idx < speed_options.length-1)
+      selected_opt_idx++;
+      d3.select("#slider_ball").attr("cx", (start_line + step*selected_opt_idx));
+  });
+
 // EVENTS OPEN
 d3.select("#setting_btn_open").on("click", function () {
   console.log("CLICK");
@@ -50,11 +102,7 @@ d3.select("#setting_btn_open").on("click", function () {
     .attr("visibility", "visible")
     .attr("d", roundedRect(-w2 + w1, 0, w2, h2, 10))
     .on("end", function () {
-      d3.select("#setting_btn_close").attr("visibility", "visible");
-      d3.select("#setting_icon_close").attr("visibility", "visible");
-      d3.select("#text_setting").attr("visibility", "visible");
-      d3.select("#text_animation").attr("visibility", "visible");
-      d3.select("#text_speed").attr("visibility", "visible");
+      d3.select("#munu_parts").selectAll("*").attr("visibility", "visible");
     });
 
   d3.select("#setting_icon_open").attr("visibility", "hidden");
@@ -72,11 +120,5 @@ d3.select("#setting_btn_close").on("click", function () {
       d3.select("#setting_btn_open").attr("visibility", "visible");
       d3.select("#setting_icon_open").attr("visibility", "visible");
     });
-
-  d3.select("#setting_icon_close").attr("visibility", "hidden");
-  d3.select(this).attr("visibility", "hidden");
-  
-  d3.select("#text_setting").attr("visibility", "hidden");
-  d3.select("#text_animation").attr("visibility", "hidden");
-  d3.select("#text_speed").attr("visibility", "hidden");
+  d3.select("#munu_parts").selectAll("*").attr("visibility", "hidden");
 });
