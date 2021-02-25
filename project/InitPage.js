7333a75fc047b718999ca8ee48f68112d53331c4
Alessandro <AlessandroCaroti@users.noreply.github.com>
2021-02-25 16:11:59 +0100

    Update InitPage.js

--- project/InitPage.js
+++ project/InitPage.js
@@ -1,23 +1,16 @@
 function init_page() {
+  init_DropDownMenu_slect2();
 
-    init_DropDownMenu_slect2();
+  // load map
+  load_map();
 
-    // load map
-    load_map();
+  // set colorscale and  legend
+  set_colorScale();
+  draw_legend();
 
-    // set colorscale and  legend
-    set_colorScale();
-    draw_legend();
-  
-    // trovare modo automatico per trovare min e max
-    init_slider(1743, 2020, 1);
-  
-    init_map_controls();
-    allDeafaultDataset();
-  
-}
+  // trovare modo automatico per trovare min e max
+  init_slider(1743, 2020, 1);
 
-function page_ready() {
-  //mappa, slider, dropdownMenù, 
-$("body").addClass("loaded");
+  init_map_controls();
+  allDeafaultDataset();
 }
