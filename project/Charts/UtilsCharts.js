15201c5fc8b9ce019b93cfc3631e9f7b8b99f5c2
simocampi <Sim0ne1997>
2021-03-01 11:29:40 +0100

    button top

--- project/Charts/UtilsCharts.js
+++ project/Charts/UtilsCharts.js
@@ -607,3 +607,21 @@ function NavBarDropDownEvt() {
     document.getElementById("bracket_drop_nav").style.transform="none";
   }
 }
+
+
+
+function scrollFunction() {
+
+  var mybutton= document.getElementById("btn-back-top");
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
