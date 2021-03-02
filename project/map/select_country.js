b667e37efd1c456c7096bfcc2411c3a9dc3981f2
Alessandro <AlessandroCaroti@users.noreply.github.com>
2021-03-02 15:37:58 +0100

    manage arrowKey to menù

--- project/map/select_country.js
+++ project/map/select_country.js
@@ -1,7 +1,10 @@
 const input_source = document.getElementById("input_countrySelection");
 const selectionCountry_list = d3.select("#country_list");
+var curr_over = null;
+var k = 0;
 
 const inputHandler = function (e) {
+  console.log(e.key);
   /*
   metches = stringSimilarity.findBestMatch(e.target.value, country_list);
   best = metches.ratings.sort((a, b) => b.rating - a.rating).slice(0, 10);
@@ -15,11 +18,34 @@ const inputHandler = function (e) {
 input_source.addEventListener("input", inputHandler);
 input_source.addEventListener("blur", async function () {
   await new Promise((r) => setTimeout(r, 500));
-  selectionCountry_list.selectAll("li").remove();
+  //selectionCountry_list.selectAll("li").remove();
 });
-input_source.addEventListener("keyup", function (event) {
+input_source.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     console.log("ENTER");
+  } else if (event.key === "ArrowDown") {
+    if (curr_over.nextElementSibling != null) {
+      d3.select(curr_over).classed("country_option_over", false);
+      curr_over = curr_over.nextElementSibling;
+      console.log(curr_over.getBoundingClientRect())
+      d3.select(curr_over).classed("country_option_over", true);
+      if(curr_over.getBoundingClientRect().y > 285){
+        k += curr_over.getBoundingClientRect().y - 285
+        document.getElementById("country_list_conteiner").scroll(0, k);
+      }
+    }
+  } else if (event.key === "ArrowUp") {
+    if (curr_over.previousElementSibling != null) {
+      d3.select(curr_over).classed("country_option_over", false);
+      curr_over = curr_over.previousElementSibling;
+      d3.select(curr_over).classed("country_option_over", true);
+      if(curr_over.getBoundingClientRect().y < 163){
+        k += curr_over.getBoundingClientRect().y - 163
+        document.getElementById("country_list_conteiner").scroll(0, k);
+      }
+    }
+  } else {
+    console.log(event.key);
   }
 });
 
@@ -68,14 +94,24 @@ function update_otionsCountry(options) {
       changeCountry(this.id);
       changeAllData(this.id);
     })
+    .on("mouseenter", function () {
+      d3.select(curr_over).classed("country_option_over", false);
+      curr_over = this;
+      d3.select(curr_over).classed("country_option_over", true);
+    })
     .html((d) => d);
+
+  selectionCountry_list
+    .select("#" + options[0])
+    .classed("country_option_over", true);
+  curr_over = document.getElementById(options[0]);
 }
 
 function find_matches(string, list_of_sting) {
   let d = [];
   string = string.toLowerCase();
   list_of_sting.forEach((el) => {
-    if ((el.toLowerCase()).includes(string)) {
+    if (el.toLowerCase().includes(string)) {
       d.push(el);
     }
   });
