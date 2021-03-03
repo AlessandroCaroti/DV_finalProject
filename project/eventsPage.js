ff2c50dbc22f40dc7524fd5f8d8d70207e30ee87
simocampi <Sim0ne1997>
2021-03-03 15:51:16 +0100

    fixed sidebar

--- project/eventsPage.js
+++ project/eventsPage.js
@@ -24,14 +24,22 @@ function NavBarDropDownEvt() {
   function scrollFunction() {
   
     var btn= document.getElementById("btn-back-top");
+    var sideDiv = document.getElementById("selectionCountry_countainer");
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
      btn.style.display = "block";
+     sideDiv.style.paddingTop = "5px";
     } else {
      btn.style.display = "none";
+     sideDiv.style.paddingTop = "80px";
     }
+
+    sideDiv.style.transition ="0.3s";
+
   }
   
   
   function topFunction() {
     document.documentElement.scrollTop = 0; 
   }
+
+
