d16355bc6279297201cc0ac174e9536ea118974c
Alessandro <alessandro.caroti.1996@gmail.com>
2021-03-07 18:06:56 +0100

    a

--- project/map/color_scale.js
+++ project/map/color_scale.js
@@ -55,7 +55,7 @@ function set_colorScale(){
 
     // generate axis
     
-    axis.attr("transform", "translate(" +   0 +","+ 600  +" ) rotate(-90 0 0)") 
+    axis.attr("transform", "translate(" + 40 +","+ 600  +" ) rotate(-90 0 0)") 
             .call(anomaly_axis);
 
     // draw bars
