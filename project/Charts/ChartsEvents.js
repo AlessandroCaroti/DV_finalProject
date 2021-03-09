f8513d9b3a20cb908fed31d8662bd2383ad1017e
simocampi <Sim0ne1997>
2021-03-09 10:00:40 +0100

    c

--- project/Charts/ChartsEvents.js
+++ project/Charts/ChartsEvents.js
@@ -1,20 +1,3 @@
-const countries = "../../../data/15_countries_list.csv";
-
-//----------------------- TAB EVENT ----------------------------------------------------
-
-function click_tab(evt, graphic_name) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(graphic_name).style.display = "block";
-  evt.currentTarget.className += " active";
-}
 
 //-------------------------LINE CHART TOOLTIP----------------------------------------
 
